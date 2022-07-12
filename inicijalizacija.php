53ece3e420ec50c5dfc6ff29b51c476820a66ee6
Tamara Vucetic <tamaravucetic98@gmail.com>
2022-07-12 10:08:02 +0200

    dodavanje konekcije

---
+++ inicijalizacija.php
@@ -0,0 +1,11 @@
+<?php
+
+error_reporting(E_ALL | E_STRICT);
+ini_set("display_errors", 0);
+ini_set("log_errors", 1);
+ini_set("error_log", "greskeFajl.log");
+
+require('dbbroker.php');
+$db = new DBBroker('localhost', 'root', '', 'odbojka');
+
+?>
